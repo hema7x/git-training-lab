f26c2d1a8da1c343684bf183a18ce355650a5f99
ibrahim hamdi <ibrahim.hamdi2115@gmail.com>
2026-07-26 11:59:08 +0300

    feat: add divide method and set owner name

--- src/main/java/com/training/App.java
+++ src/main/java/com/training/App.java
@@ -3,13 +3,13 @@ package com.training;
 public class App {
     public static void main(String[] args) {
         // DO NOT ADD A NEW LINE - REPLACE THE LINE BELOW WITH YOUR OWN MESSAGE
-        System.out.println("Welcome to Git Training Lab");
+        System.out.println("Welcome from Intern B - <ibrahim hamdi mahmoud > ");
 
         Calculator calculator = new Calculator();
         System.out.println("2 + 3 = " + calculator.add(2, 3));
         System.out.println("10 - 4 = " + calculator.subtract(10, 4));
 
         // DO NOT ADD A NEW LINE - REPLACE THE LINE BELOW WITH YOUR OWN OPERATION
-        System.out.println("Extra operation result = not implemented yet");
+        System.out.println("20 / 4 " + calculator.divide(20 , 4 ));
     }
 }
